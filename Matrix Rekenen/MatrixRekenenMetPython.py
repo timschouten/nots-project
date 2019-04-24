f7e1d8dffbbc635cab8da54c25f6e4766caf9cb4
Unknown <yigitbaylann@gmail.com>
2019-04-24 10:28:04 +0200

    WIP - application is now more dynamic

--- Matrix Rekenen/MatrixRekenenMetPython.py
+++ Matrix Rekenen/MatrixRekenenMetPython.py
@@ -1,37 +1,74 @@
-a = [[1, 2],
-     [3, 2]]
+a = [[12, 2, 12],
+     [3, 32, 3],
+     [3, 32, 3],
+     [3, 32, 3]]
 
-b = [[3, 2],
-     [1, 2]]
+b = [[33, 21, 32, 4],
+     [8, 25, 325, 6],
+     [8, 25, 53, 7]]
 
 final = []
 
 index = 0
 
-newRow = []
-for row in a:
-    column = []
-
-    i = 0
-    for bRow in b:
-        while i < len(bRow):
-            i += 1
-            column.append(bRow[index])
-    index = index + 1
-
-    print(column)
-
-    secondIndex = 0
-    newSum = []
-    for bColumn in column:
-        print(row[secondIndex] * bColumn, secondIndex)
-        newSum.append(row[secondIndex] * bColumn)
-        secondIndex = secondIndex + 1
+def index_exists(ls, i):
+    return (0 <= i < len(ls)) or (-len(ls) <= i < 0)
 
 
+def get_columns_from_array(array):
+    columns = []
+    for bRow in array:
+        for i in range(len(bRow)):
+            if index_exists(columns, i):
+                columns[i].append(bRow[i])
+            else:
+                column = [bRow[i]]
+                columns.append(column)
+    return columns
+
+
+def multiply_matrix(row, columns):
+    total = []
+    column_index = 0
+    for r in row:
+        for column in columns:
+            total.append(r * column[column_index])
+        column_index += 1
+    return total
+
+
+def calculate_new_matrix(array_of_sum):
+    newRow = []
     value = 0
-    for sum in newSum:
-        value = value + sum
+    secondValue = 0
+    thirdValue = 0
+    forthValue = 0
+    same = 0
+
+    for sum in array_of_sum:
+        if same == 0:
+            value = value + sum
+            same = 1
+        elif same == 1:
+            secondValue = secondValue + sum
+            same = 2
+        elif same == 2:
+            thirdValue = thirdValue + sum
+            same = 3
+        else:
+            forthValue = forthValue + sum
+            same = 0
 
     newRow.append(value)
-    print(newRow, "END")
+    newRow.append(secondValue)
+    newRow.append(thirdValue)
+    newRow.append(forthValue)
+    final.append(newRow)
+
+
+for row in a:
+    columns = get_columns_from_array(b)
+    newSum = multiply_matrix(row, columns)
+    calculate_new_matrix(newSum)
+
+print("END", final)
