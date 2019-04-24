3d5a196f6d586821214e66e8d6917a03945afc2c
Tim Schouten <tim.schouten@outlook.com>
2019-04-24 14:54:30 +0200

    Matrix calculator

---
+++ Proof of concepts/Matrix Calculator C# - Tim/MatrixRekenen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MatrixRekenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int columns = 3;
+            const int rows = 3;
+
+            int[,] matrix1 = new int[rows, columns] { { 4, 6, 1 }, { 2, 1, 3 }, { 2, 1, 3 } };
+            int[,] matrix2 = new int[columns, rows] { { 1, 5, 2 }, { 4, 5, 4 }, { 1, 3, 8 } };
+            int[,] result = new int[rows, rows];
+
+            for (int column = 0; column < columns; column++)
+                for (int row = 0; row < rows; row++)
+                    for (int i = 0; i < rows; i++)
+                        result[column, row] += matrix1[column, i] * matrix2[i, row];
+
+            foreach (int item in result)
+                Console.WriteLine(item);
+        }
+    }
+}
