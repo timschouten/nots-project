3b80ca635d283b12bcd6e6c89ed497823cb693d5
Unknown <yigitbaylann@gmail.com>
2019-04-24 11:27:26 +0200

    Dynamic matrix calculator with python

--- Matrix Rekenen/MatrixRekenenMetPython.py
+++ Matrix Rekenen/MatrixRekenenMetPython.py
@@ -1,20 +1,18 @@
-a = [[12, 2, 12],
-     [3, 32, 3],
-     [3, 32, 3],
-     [3, 32, 3]]
+first = [[1, 2, 3],
+     [3, 2, 1],
+     [1, 2, 3]]
 
-b = [[33, 21, 32, 4],
-     [8, 25, 325, 6],
-     [8, 25, 53, 7]]
+second = [[1, 2],
+         [1, 2],
+         [1, 2]]
 
-final = []
-
-index = 0
 
 def index_exists(ls, i):
     return (0 <= i < len(ls)) or (-len(ls) <= i < 0)
 
 
+# Creating columns from a multi dimensional array
+# Returns the columns as a multi dimensional array
 def get_columns_from_array(array):
     columns = []
     for bRow in array:
@@ -27,48 +25,40 @@ def get_columns_from_array(array):
     return columns
 
 
+# Multiplies the row with the columns
+# Returns the total of the row as an array
 def multiply_matrix(row, columns):
     total = []
     column_index = 0
     for r in row:
+        new = []
         for column in columns:
-            total.append(r * column[column_index])
+            new.append(r * column[column_index])
+        total.append(new)
         column_index += 1
     return total
 
 
 def calculate_new_matrix(array_of_sum):
-    newRow = []
-    value = 0
-    secondValue = 0
-    thirdValue = 0
-    forthValue = 0
-    same = 0
+    values = []
 
-    for sum in array_of_sum:
-        if same == 0:
-            value = value + sum
-            same = 1
-        elif same == 1:
-            secondValue = secondValue + sum
-            same = 2
-        elif same == 2:
-            thirdValue = thirdValue + sum
-            same = 3
-        else:
-            forthValue = forthValue + sum
-            same = 0
-
-    newRow.append(value)
-    newRow.append(secondValue)
-    newRow.append(thirdValue)
-    newRow.append(forthValue)
-    final.append(newRow)
+    for row in array_of_sum:
+        for i in range(len(array_of_sum)):
+            if index_exists(values, i):
+                values[i] = values[i] + row[i]
+            else:
+                if index_exists(row, i):
+                    values.append(row[i])
+    return values
 
 
-for row in a:
-    columns = get_columns_from_array(b)
-    newSum = multiply_matrix(row, columns)
-    calculate_new_matrix(newSum)
+def multiply(a, b):
+    final = []
+    for row in a:
+        columns = get_columns_from_array(b)
+        new_sum = multiply_matrix(row, columns)
+        final.append(calculate_new_matrix(new_sum))
+    return final
 
-print("END", final)
+
+print("RESULT", multiply(first, second))
