73901227b31bac8c9764a3b63b14e7bc700bc2cd
Max Schulz <info@maxschulz.com>
2019-05-13 12:03:44 +0200

    Benchmarks added to correct dir

---
+++ Proof of concepts/Benchmark/Max (ML.Net)/BenchmarkMLNet/WalkDataMLNet/AIClass.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WalkDataMLNet.Classes;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace WalkDataMLNet
+{
+    public static class AIClass
+    {
+        public static void DoAIStuff()
+        {
+            MLContext mlContext = new MLContext();
+            TrainTestData splitDataView = LoadData(mlContext); // <-- Data uitgelezen uit Excel?
+
+            //var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "WalkData" })
+            var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "Score" })
+               .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Label", maximumNumberOfIterations: 100));
+
+            IDataView trainingData = mlContext.Data.LoadFromEnumerable(GetWalkDataRecords());
+
+            var model = pipeline.Fit(trainingData);
+
+            var testWalkDataView = mlContext.Data.LoadFromEnumerable(GetWalkDataRecords());
+            var testWalkData = model.Transform(testWalkDataView);
+
+            //var metrics = mlContext.Regression.Evaluate(testWalkDataView, labelColumnName: "Label");
+            var metrics = mlContext.Regression.Evaluate(testWalkDataView, labelColumnName: "Label");
+
+            Console.WriteLine($"R^2: {metrics.RSquared:0.##}");
+            Console.WriteLine($"RMS error: {metrics.RootMeanSquaredError:0.##}");
+
+            //ITransformer transformerModel = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            //Evaluate(mlContext, transformerModel, splitDataView.TestSet);
+            //UseModelWithSingleItem(mlContext, model);
+            //UseModelWithBatchItems(mlContext, model);
+        }
+
+        /// <summary>
+        /// Load data 
+        /// </summary>
+        /// <param name="mlContext"></param>
+        /// <returns></returns>
+        private static TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromEnumerable(GetWalkDataRecords()); 
+                // mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            
+            // 0.2 stands for the percentage of the ammount of trainingdata in this case 20%
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            return splitDataView;
+        }
+
+        private static List<WalkDataRecord> GetWalkDataRecords()
+        {
+            var csv = new List<WalkDataRecord>();
+            var lines = File.ReadAllLines(@"Data/datasetSubsetStap.csv");
+            foreach (string line in lines)
+            {
+                if (line != lines.First())
+                {
+                    WalkDataRecord wdr = new WalkDataRecord();
+                    string[] temp = line.Split(';');
+                    wdr.Moment = 0; // Convert.ToSingle(float.Parse(temp[0])); //Convert.ToDateTime(temp[0]);
+                    wdr.AxisData = new float[] { float.Parse(temp[1]), float.Parse(temp[2]), float.Parse(temp[3]) };
+                    csv.Add(wdr);
+
+                    // Console.WriteLine("DateTime = " + wdr.Moment.ToString() + " // Axis = " + wdr.AxisData[0] + ", " + wdr.AxisData[1] + ", " + wdr.AxisData[2]);
+                }
+            }
+            return csv;
+        }
+    }
+}
---
+++ Proof of concepts/Benchmark/Max (ML.Net)/BenchmarkMLNet/WalkDataMLNet/ToDo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalkDataMLNet
+{
+    class ToDo
+    {
+        /**
+        TrainTestData splitDataView = LoadData(mlContext);
+        ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+        Evaluate(mlContext, model, splitDataView.TestSet);
+        UseModelWithSingleItem(mlContext, model);
+        UseModelWithBatchItems(mlContext, model);
+         * 
+         * 
+        public static TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromEnumerable<WalkDataRecord>(GetWalkDataRecords());
+            // 0.2 stands for the percentage of the ammount of trainingdata in this case 20%
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            return splitDataView;
+        }
+
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: "WalkData")
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "WalkData", featureColumnName: "WalkData"));
+
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            return model;
+        }
+
+        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+
+        private static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+        {
+            PredictionEngine<WalkDataRecord, WalkRecordPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<WalkDataRecord, WalkRecordPrediction>(model);
+            WalkDataRecord sampleStatement = new WalkDataRecord
+            {
+                AxisData = new float[] { 0.5f, 0.5f, 0.5f }
+            };
+            var resultprediction = predictionFunction.Predict(sampleStatement);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Prediction: {(Convert.ToBoolean(resultprediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultprediction.Probability} ");
+
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+        }
+
+        public static void UseModelWithBatchItems(MLContext mlContext, ITransformer model)
+        {
+            IEnumerable<WalkDataRecord> sentiments = new[]
+            {
+                new WalkDataRecord
+                {
+                    AxisData = new float[] { 0.5f, 0.5f, 0.5f }
+                },
+                new WalkDataRecord
+                {
+                    AxisData = new float[] { 1f, 1f, 1f }
+                }
+            };
+
+            IDataView batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+
+            IDataView predictions = model.Transform(batchComments);
+
+            // Use model to predict whether comment data is Positive (1) or Negative (0).
+            IEnumerable<WalkRecordPrediction> predictedResults = mlContext.Data.CreateEnumerable<WalkRecordPrediction>(predictions, reuseRowObject: false);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+
+            foreach (WalkRecordPrediction prediction in predictedResults)
+            {
+                Console.WriteLine($"Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability: {prediction.Probability} ");
+            }
+            Console.WriteLine("=============== End of predictions ===============");
+        }
+         */
+    }
+}
